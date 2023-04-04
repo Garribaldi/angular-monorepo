b72a4265d6f56b14530f89e1e1dc83cf837d0ef5
Grieger <andre.grieger@unisys.com>
2023-04-04 10:24:37 +0200

    initial commit

---
+++ apps/demo/src/app/app.component.spec.ts
@@ -0,0 +1,26 @@
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { NxWelcomeComponent } from "./nx-welcome.component";
+import { RouterTestingModule } from "@angular/router/testing";
+
+describe("AppComponent", () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, NxWelcomeComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should render title", () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector("h1")?.textContent).toContain("Welcome demo");
+    expect(app.title).toEqual("demo");
+  });
+});
