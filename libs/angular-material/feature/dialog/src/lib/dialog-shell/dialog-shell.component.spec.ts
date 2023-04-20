1dac78ce77a9ecda27305fa23135fc904398a6cd
GriegerA <andre.grieger@unisys.com>
2023-04-20 16:13:14 +0200

    fix unit tests

--- libs/angular-material/feature/dialog/src/lib/dialog-shell/dialog-shell.component.spec.ts
+++ libs/angular-material/feature/dialog/src/lib/dialog-shell/dialog-shell.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DialogShellComponent } from './dialog-shell.component';
+import { MockModule } from "ng-mocks";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
 
 describe('DialogShellComponent', () => {
   let component: DialogShellComponent;
@@ -7,6 +11,11 @@ describe('DialogShellComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        MockModule(MatDialogModule),
+        MockModule(MatFormFieldModule),
+        MockModule(FormsModule)
+      ],
       declarations: [DialogShellComponent],
     }).compileComponents();
 
--- libs/angular-material/feature/dialog/src/lib/dialog/dialog.component.spec.ts
+++ libs/angular-material/feature/dialog/src/lib/dialog/dialog.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DialogComponent } from './dialog.component';
+import { MockModule, MockProvider } from "ng-mocks";
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import { DialogData } from "./dialog-data.model";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
 
 describe('DialogComponent', () => {
   let component: DialogComponent;
@@ -7,7 +12,16 @@ describe('DialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        MockModule(MatDialogModule),
+        MockModule(MatFormFieldModule),
+        MockModule(FormsModule)
+      ],
       declarations: [DialogComponent],
+      providers: [
+        MockProvider(MatDialogRef),
+        {provide: MAT_DIALOG_DATA, useFactory: () => ({brand: '', type: ''} as DialogData)}
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DialogComponent);
