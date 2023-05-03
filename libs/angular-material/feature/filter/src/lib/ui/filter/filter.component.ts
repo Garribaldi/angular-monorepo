cf31efd61fdcacfcde6245b87da8523e24cf3301
GriegerA <andre.grieger@unisys.com>
2023-05-03 07:49:53 +0200

    improve generic types, add unsubscribe

--- libs/angular-material/feature/filter/src/lib/ui/filter/filter.component.ts
+++ libs/angular-material/feature/filter/src/lib/ui/filter/filter.component.ts
@@ -4,19 +4,21 @@ import {
   EventEmitter,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   Output,
   SimpleChanges,
   ViewChild
 } from '@angular/core';
 import { FormControl, FormGroup } from "@angular/forms";
+import { Subject, takeUntil } from "rxjs";
 
 @Component({
   selector: 'local-angular-material-filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss'],
 })
-export class FilterComponent<T extends { [key: string]: any }> implements OnInit, OnChanges {
+export class FilterComponent<T extends {[key: string]: K}, K extends string | number> implements OnInit, OnChanges, OnDestroy {
 
   @ViewChild('inputFilter', {static: true}) inputFilter!: ElementRef<HTMLInputElement>;
 
@@ -24,18 +26,21 @@ export class FilterComponent<T extends { [key: string]: any }> implements OnInit
   @Input() filterColumn = '';
 
   filteredData: T[] = [];
-  filteredFilterValues: Array<string | number> = [];
+  filteredFilterValues: Array<K> = [];
 
   formGroup: FormGroup = new FormGroup({
     selectedFilter: new FormControl("")
   });
 
-  private unfilteredFilterValues: Array<string | number> = [];
+  private unfilteredFilterValues: Array<K> = [];
+  private unsubscribe = new Subject<void>();
 
   @Output() dataFiltered = new EventEmitter<T[]>();
 
   ngOnInit() {
-    this.formGroup.valueChanges.subscribe(
+    this.formGroup.valueChanges
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(
       changed => {
         const {selectedFilter} = changed;
         const unfiltered = this.unfilteredData ?? [];
@@ -68,7 +73,12 @@ export class FilterComponent<T extends { [key: string]: any }> implements OnInit
   lookup(target: EventTarget | null) {
     const searchPhrase = (target as HTMLInputElement).value.toLowerCase();
 
-    this.filteredFilterValues = this.unfilteredFilterValues.filter(value => (value as string).toLowerCase().indexOf(searchPhrase) > -1);
+    this.filteredFilterValues = this.unfilteredFilterValues.filter(value => value.toString().toLowerCase().indexOf(searchPhrase) > -1);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
   clear(): void {
