8d36e46e30b52277442f52ba7a844aa220e3877d
GriegerA <andre.grieger@unisys.com>
2023-05-12 14:15:55 +0200

    add unit tests for data grid component

--- libs/angular-material/feature/data-grid/src/lib/data-grid/data-grid.component.spec.ts
+++ libs/angular-material/feature/data-grid/src/lib/data-grid/data-grid.component.spec.ts
@@ -5,12 +5,23 @@ import { SelectedFilterStateService } from "../selected-filter-state.service";
 import { DataSourceService } from "../data-source.service";
 import { of } from "rxjs";
 import { DataGridChipsBarComponent } from "../data-grid-chips-bar/data-grid-chips-bar.component";
+import { Filter } from "../models/filter.model";
 
-type TestData = { name: string };
+type TestData = { column1: string, columns2: string, column3: string };
 
 describe('DataGridComponent', () => {
   let component: DataGridComponent<TestData>;
   let fixture: ComponentFixture<DataGridComponent<TestData>>;
+
+  let selectedFilterStateService: SelectedFilterStateService;
+  let dataSourceService: DataSourceService<TestData>;
+
+  let spyOnFiltered: jest.SpyInstance;
+
+  const testDataSource: TestData[] = [{column1: 'test 1', columns2: 'test 2', column3: 'test 3'}];
+
+  const testFilter = {id: 'test-filter-1'} as Filter;
+  const testFilterGroup = new Map<string, Filter[]>().set('column1', [testFilter]);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,17 +30,55 @@ describe('DataGridComponent', () => {
         MockComponent(DataGridChipsBarComponent)
       ],
       providers: [
-        MockProvider(SelectedFilterStateService, {selectedFilter$: of(new Map())}),
-        MockProvider(DataSourceService)
+        MockProvider(SelectedFilterStateService, {
+          selectedFilter$: of(testFilterGroup),
+          removeFilter: jest.fn(),
+          removeAllFilters: jest.fn()
+        }),
+        MockProvider(DataSourceService, {
+          filter: jest.fn()
+        })
       ]
     }).compileComponents();
 
+    jest.clearAllMocks();
+
+    selectedFilterStateService = TestBed.inject(SelectedFilterStateService);
+    dataSourceService = TestBed.inject(DataSourceService);
+
     fixture = TestBed.createComponent(DataGridComponent<TestData>);
+
     component = fixture.componentInstance;
+    component.dataSource = testDataSource;
+
+    spyOnFiltered= jest.spyOn(component.filtered, 'next');
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.filters).toEqual(testFilterGroup);
+    expect(dataSourceService.filter).toHaveBeenCalledWith(testFilterGroup);
+    expect(dataSourceService.dataSource).toEqual(testDataSource);
+    expect(spyOnFiltered).toHaveBeenCalled();
+  });
+
+  describe('removeFilter()', () => {
+
+    it('should remove single filter', () => {
+      component.removeFilter(testFilter);
+
+      expect(selectedFilterStateService.removeFilter).toHaveBeenCalledWith(testFilter);
+    });
+  });
+
+  describe('removeAllFilter()', () => {
+
+    it('should remove all filter', () => {
+      component.removelAllFilter();
+
+      expect(selectedFilterStateService.removeAllFilters).toHaveBeenCalled();
+    });
   });
 });
--- libs/shared/utils/src/index.ts
+++ libs/shared/utils/src/index.ts
@@ -2,6 +2,9 @@ export * from './lib/shared-utils.module';
 
 export * from './lib/directives/ng-template-name.directive';
 
+export * from './lib/pipes/get-template-ref.pipe';
+export * from './lib/pipes/to-slash-case.pipe';
+
 export * from './lib/validators/validate-password-strength';
 export * from './lib/helpers/type-guards';
 export * from './lib/helpers/utilities';
