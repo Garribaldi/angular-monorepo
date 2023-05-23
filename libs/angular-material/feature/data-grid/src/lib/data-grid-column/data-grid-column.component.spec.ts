86269f13ffaf3dd128a9ba5519091c48ec7c6baa
GriegerA <andre.grieger@unisys.com>
2023-05-23 11:40:00 +0200

    increase test coverage

--- libs/angular-material/feature/data-grid/src/lib/data-grid-column/data-grid-column.component.spec.ts
+++ libs/angular-material/feature/data-grid/src/lib/data-grid-column/data-grid-column.component.spec.ts
@@ -6,9 +6,10 @@ import { FilterType } from "../models/filter-type.model";
 import { DataGridCheckFilterComponent } from "../data-grid-check-filter/data-grid-check-filter.component";
 import { DataGridDateFilterComponent } from "../data-grid-date-filter/data-grid-date-filter.component";
 import { Filter } from "../models/filter.model";
-import { of } from "rxjs";
+import { Subject } from "rxjs";
 import { CypressSelectorDirective, ToSlashCasePipe } from "@local/shared/utils";
 import { SelectedFilterStateService } from "../selected-filter-state.service";
+import { DateFilter } from "../models/date-filter.model";
 
 type TestData = { name: string };
 
@@ -18,15 +19,24 @@ describe('DataGridColumnComponent', () => {
 
   let selectedFilterServicec: SelectedFilterStateService;
 
-  const testFilter: Filter[] = [{
-    id: '12345',
-    column: 'name',
-    type: FilterType.CHECK_FILTER,
-    value: 'Test Value',
-    displayValue: 'Test Value',
-    label: 'Filter Test',
-    hitCount: 2
-  } as Filter];
+  const currentColumn = 'test-column';
+
+  const testCheckFilter: Filter[] = [
+    {
+      id: '12345',
+      column: currentColumn,
+      type: FilterType.CHECK_FILTER,
+      value: 'Test Value',
+      displayValue: 'Test Value',
+      label: 'Check Label',
+      hitCount: 2
+    } as Filter
+  ];
+
+  const testDateFilter = new DateFilter({value: {from: null, to: null}, label: 'Date Label', column: currentColumn});
+
+  const changeDataSource = new Subject<void>();
+  const removedFilter = new Subject<Filter[]>();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -38,11 +48,11 @@ describe('DataGridColumnComponent', () => {
       ],
       providers: [
         MockProvider(DataSourceService, {
-          getCheckFilter: jest.fn().mockReturnValue(testFilter),
-          dataSourceChanged$: of(void '')
+          getCheckFilter: jest.fn().mockReturnValue(testCheckFilter),
+          dataSourceChanged$: changeDataSource.asObservable()
         }),
         MockProvider(SelectedFilterStateService, {
-          removedFilter$: of([]),
+          removedFilter$: removedFilter.asObservable(),
           addFilter: jest.fn(),
           removeFilter: jest.fn(),
           removeFilterByColumn: jest.fn()
@@ -55,34 +65,106 @@ describe('DataGridColumnComponent', () => {
     fixture = TestBed.createComponent(DataGridColumnComponent<TestData>);
     component = fixture.componentInstance;
 
-    component.column = 'name';
-    component.label = 'Test'
-    component.type = FilterType.CHECK_FILTER;
+    component.column = currentColumn;
+    component.label = 'Test Label'
+    component.type = FilterType.DATE_FILTER;
 
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+    expect(component.checkFilter).toEqual([]);
+    expect(component.dateFilter).toBeUndefined();
+  });
+
+  describe('dataSourceChanged$', () => {
+
+    it('should have valid check filter', () => {
+      component.type = FilterType.CHECK_FILTER;
+      fixture.detectChanges();
+
+      changeDataSource.next();
+
+      expect(component.checkFilter).toEqual(testCheckFilter);
+      expect(component.dateFilter).toBeUndefined();
+    });
+
+    it('should have valid date filter', () => {
+      component.type = FilterType.DATE_FILTER;
+      fixture.detectChanges();
+
+      changeDataSource.next();
+
+      expect(component.checkFilter).toEqual([]);
+      expect(component.dateFilter).toBeInstanceOf(DateFilter);
+      expect(component.dateFilter?.label).toEqual(component.label);
+      expect(component.dateFilter?.column).toEqual(component.column);
+    });
+  });
+
+  describe('removedFilter$', () => {
+
+    it('should remove filter from current column', () => {
+      component.type = FilterType.CHECK_FILTER;
+      fixture.detectChanges();
+      changeDataSource.next();
+
+      removedFilter.next(testCheckFilter);
+
+      expect(component.removedFilter).toEqual(testCheckFilter);
+    });
   });
 
   describe('addFilter()', () => {
 
-    it('', () => {});
+    let spyOnAddFilter: jest.SpyInstance;
+
+    beforeEach(() => spyOnAddFilter = jest.spyOn(selectedFilterServicec, 'addFilter'));
+
+    it('should add filter', () => {
+      component.addFilter(testDateFilter)
+
+      expect(spyOnAddFilter).toHaveBeenCalledWith(testDateFilter);
+    });
   });
 
   describe('removeFilter()', () => {
 
-    it('', () => {});
+    let spyOnRemoveFilter: jest.SpyInstance;
+
+    beforeEach(() => spyOnRemoveFilter = jest.spyOn(selectedFilterServicec, 'removeFilter'));
+
+    it('should remove filter', () => {
+      component.removeFilter(testCheckFilter[0]);
+
+      expect(spyOnRemoveFilter).toHaveBeenCalledWith(testCheckFilter[0]);
+    });
   });
 
   describe('removeFilterByColumn()', () => {
 
-    it('', () => {});
+    let spyOnRemoveFilterByColumn: jest.SpyInstance;
+
+    beforeEach(() => spyOnRemoveFilterByColumn = jest.spyOn(selectedFilterServicec, 'removeFilterByColumn'));
+
+    it('should remove filter for current column', () => {
+      component.removeFilterByColumn();
+
+      expect(spyOnRemoveFilterByColumn).toHaveBeenCalledWith(component.column);
+    });
   });
 
   describe('updateFilterByColumn()', () => {
 
-    it('', () => {});
+    let spyOnUpdateFilterByColumn: jest.SpyInstance;
+
+    beforeEach(() => spyOnUpdateFilterByColumn = jest.spyOn(selectedFilterServicec, 'updateFilterByColumn'));
+
+    it('should update filter for current column', () => {
+      component.updateFilterByColumn(testDateFilter);
+
+      expect(spyOnUpdateFilterByColumn).toHaveBeenCalledWith(testDateFilter, component.column);
+    });
   });
 });
