16bd5523ed185a71623c70c3593cce9d1bf5921b
Andre <73430445+Garribaldi@users.noreply.github.com>
2023-05-23 14:11:51 +0200

    Delete env.js

--- apps/demo/src/assets/env.js
+++
@@ -1,5 +0,0 @@
-(function (window) {
-  window["env"] = window["env"] || {};
-  window["env"].API_BACKEND_URL = 'http://localhost:8080/tomate'; // not updated, for local testing
-  window["env"].EXTERNAL_INTEGRATION_URL = 'https://www.integration.com/easy'; // not updated, for local testing
-})(this);
