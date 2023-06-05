9fc1a79f34ae26703c9fbb3795ddacc720616d18
GriegerA <andre.grieger@unisys.com>
2023-06-05 11:56:08 +0200

    add jest-junit configuration

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,18 @@
+const {readCachedProjectConfiguration } = require('nx/src/project-graph/project-graph');
 const nxPreset = require("@nrwl/jest/preset").default;
 
-module.exports = { ...nxPreset };
+const currentModuleName = process.env.NX_TASK_TARGET_PROJECT;
+const workspaceRoot = process.env.NX_WORKSPACE_ROOT;
+
+const configuration = readCachedProjectConfiguration(currentModuleName);
+const moduleFolder = configuration.sourceRoot.replace('/src', '');
+const junitOutputDirectory = `${workspaceRoot}/coverage/${moduleFolder}`;
+
+module.exports = {
+  ...nxPreset,
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+  reporters: [
+    'default',
+    ['jest-junit', {outputDirectory: junitOutputDirectory, outputName: 'junit.xml'}]
+  ]
+};
