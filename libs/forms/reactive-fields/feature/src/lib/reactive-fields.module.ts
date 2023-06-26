79c6e0cd1457600a55ba3d38cb8e0213be83268c
GriegerA <andre.grieger@unisys.com>
2023-06-26 10:34:30 +0200

    implement email field

--- libs/forms/reactive-fields/feature/src/lib/reactive-fields.module.ts
+++ libs/forms/reactive-fields/feature/src/lib/reactive-fields.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PasswordFieldComponent } from './password-field/password-field.component';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { SharedUtilsModule } from "@local/shared/utils";
-import { ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { SharedUtilsModule } from '@local/shared/utils';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EmailFieldComponent } from './email-field/email-field.component';
+import { TextFieldComponent } from './text-field/text-field.component';
+import { SelectFieldComponent } from './select-field/select-field.component';
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
   imports: [
@@ -12,13 +16,20 @@ import { ReactiveFormsModule } from "@angular/forms";
     MatFormFieldModule,
     MatInputModule,
     SharedUtilsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatSelectModule,
   ],
   declarations: [
-    PasswordFieldComponent
+    PasswordFieldComponent,
+    EmailFieldComponent,
+    TextFieldComponent,
+    SelectFieldComponent
   ],
   exports: [
-    PasswordFieldComponent
-  ]
+    PasswordFieldComponent,
+    EmailFieldComponent,
+    TextFieldComponent,
+    SelectFieldComponent
+  ],
 })
 export class ReactiveFieldsModule {}
